31c817fa47c4347a47ef1203b573af9a1b2a85d4
jeansanch <jean.sanchuki@gmail.com>
2022-08-02 17:22:05 -0300

    add/new colors, fix/end of line check

--- retas/linhas.js
+++ retas/linhas.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('mazeCanvas');
 const cmat = canvas.getContext('2d');
+let lines = [];
 
 class Mouse{
 
@@ -33,19 +34,23 @@ class Line{
        
     }
 
-    closestPoint(x, y){
+    closestPoint(x, y, get){
         var point = Math.pow(x-this.x, 2)+Math.pow(y-this.y, 2);
         var point2 = Math.pow(x-this.xi, 2)+Math.pow(y-this.yi, 2);
         if (point < point2){
-            drawCircle(this.x, this.y);
+            if(get)
+                return point;
+            drawCircle(this.x, this.y, true);
         }
-        else
-            drawCircle(this.xi, this.yi);
-
+        else{
+            if(get)
+                return point2;
+            drawCircle(this.xi, this.yi, true);
+        }
     }
 
     closestLine(x, y){
-        var offset = 5
+        var offset = 0;
         var dxc = mouse.x - this.x;
         var dyc = mouse.y - this.y;
         var dxl = this.xi - this.x;
@@ -59,29 +64,27 @@ class Line{
         if(dyc >= -offset && dyc <= offset)
             dyc = 0
         var cross = dxc * dyl - dyc * dxl;
-        if(cross != 0){
-            
-        }
-        else{
+        if(cross == 0){
             cmat.beginPath();
             cmat.strokeStyle = "green";
             cmat.moveTo(this.x, this.y);
             cmat.lineTo(this.xi, this.yi);
             cmat.stroke();
+
+            drawCircle(mouse.x, mouse.y);
         }
     }
 }
 
 function redraw(){
     cmat.clearRect(0, 0, canvas.width, canvas.height);
-    line.drawLine();
+    lines.forEach(i => {
+        i.drawLine();
+    })
 }
 
-function drawCircle(x, y){
-    if(mouseDown)
-        cmat.fillStyle = "green"
-    else 
-        cmat.fillStyle = "red"
+function drawCircle(x, y, end){
+    cmat.fillStyle = end ? clicked ? "green": "red" : "yellow"
     cmat.beginPath();
     cmat.arc(x, y, 5, 0, 2 * Math.PI);
     cmat.fill();
@@ -95,50 +98,45 @@ function getMousePos(canvas, evt){
   };
 }
 
+small = -1;
+
 canvas.addEventListener('mousemove', function(evt){
     var mousePos = getMousePos(canvas, evt);
     mouse.x = mousePos.x;
     mouse.y = mousePos.y;
-    line.drawClosest(mouse.x, mouse.y)
+    smaller = Infinity;
+    count = 0;
+    lines.forEach(i => {
+        value = i.closestPoint(mouse.x, mouse.y, true)
+        if(value < smaller)
+            smaller = value;
+            small = count;
+        count+=1;
+    })
+    lines[small].drawClosest(mouse.x, mouse.y);
+
+    
 }, false);
 
 canvas.addEventListener('contextmenu', function(evt){
-    var offset = 5
-    var dxc = mouse.x - line.x;
-    var dyc = mouse.y - line.y;
-    var dxl = line.xi - line.x;
-    var dyl = line.yi - line.y;
-    if(dxc >= -offset && dxc <= offset)
-        dxc = 0;
-    if(dyl >= -offset && dyl <= offset)
-        dyl = 0
-    if(dxl >= -offset && dxl <= offset)
-        dxl = 0;
-    if(dyc >= -offset && dyc <= offset)
-        dyc = 0
-    console.log(dxc + ", " +dyl + ", " +dyc + ", " +dxl)
-    var cross = dxc * dyl - dyc * dxl;
-    if(cross != 0){
-        console.log(false)
-    }
-    else 
-        console.log(true)
+    console.log("right")
 }, false);
 
-let mouseDown = false;
-document.body.onmousedown = () => {
-  mouseDown = true;
-  line.drawClosest(mouse.x, mouse.y)
-};
-document.body.onmouseup = () => {
-  mouseDown = false;
-};
+    //CORTAR LINHA AO MEIO
+clicked = false;
 
+canvas.addEventListener('click', function(evt){
+    console.log("click")
+    clicked = !clicked;
+    lines[small].drawClosest(mouse.x, mouse.y);
+
+    //MOVER LINHA
+}, false)
 
 function init(){
-    line = new Line(canvas.width/4, canvas.height/2, canvas.width/4*3, canvas.height/2)   
+    lines.push(new Line(canvas.width/4, canvas.height/2, canvas.width/4*3, canvas.height/2))
     mouse = new Mouse(0,0);
-    line.drawLine()
+    lines[0].drawLine();
 }
 
 init()
